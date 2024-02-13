fa477fea5ab2bb999923832cbffc2a34a37baece
raulxdd <raul.babajev@gmail.com>
2024-02-13 19:43:38 +0200

    2nd try

--- app.js
+++ app.js
@@ -7,16 +7,41 @@ const bookList = document.querySelector('#book-list');
 
 let myLibrary = [];
 
-function Book(title, author, description, status) {
+function Book(title, author, description, status, tearRating, spiceRating, excitementRating, overallRating) {
   this.title = title;
   this.author = author;
-  this.descripion = description;
+  this.description = description;
   this.status = status;
+  this.tearRating = tearRating;
+  this.spiceRating = spiceRating;
+  this.excitementRating = excitementRating;
+  this.overallRating = overallRating;
 }
 
+
 function addBookToLibrary() {
-  const newBook = new Book(titleInput.value, authorInput.value,
-    descInput.value, statusInput.value);
+
+  const selectedTearRating = document.querySelector('input[name="tear-rating"]:checked');
+  const selectedSpiceRating = document.querySelector('input[name="spice-rating"]:checked');
+  const selectedExcitementRating = document.querySelector('input[name="excitement-rating"]:checked');
+  const selectedOverallRating = document.querySelector('input[name="overall-rating"]:checked');
+
+  const tearRating = selectedTearRating ? parseInt(selectedTearRating.value) : 0;
+  const spiceRating = selectedSpiceRating ? parseInt(selectedSpiceRating.value) : 0;
+  const excitementRating = selectedExcitementRating ? parseInt(selectedExcitementRating.value) : 0;
+  const overallRating = selectedOverallRating ? parseInt(selectedOverallRating.value) : 0;
+
+
+  const newBook = new Book(
+    titleInput.value,
+    authorInput.value,
+    descInput.value,
+    statusInput.value,
+    tearRating,
+    spiceRating,
+    excitementRating,
+    overallRating,
+  );
   myLibrary.push(newBook);
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
@@ -34,28 +59,24 @@ function displayBooks() {
     author.textContent = `By ${book.author}`;
 
     const description = document.createElement('p');
-    description.textContent = book.descripion;
+    description.textContent = book.description;
     description.classList.add('description');
 
     const status = document.createElement('p');
     status.classList.add('status');
+    status.textContent = `Status: ${book.status}`;
 
-    switch (book.status) {
-      case 'read':
-        status.textContent = 'Loetud';
-        status.style.color = 'green';
-        break;
-      case 'to-read':
-        status.textContent = 'Plaanis lugeda';
-        status.style.color = 'red';
-        break;
-      case 'reading':
-        status.textContent = 'Loen';
-        status.style.color = 'orange';
-        break;
-      default:
-        break;
-    }
+    const tearRatingElement = document.createElement('p');
+    tearRatingElement.textContent = `Tear Rating: ${book.tearRating}`;
+
+    const spiceRatingElement = document.createElement('p');
+    spiceRatingElement.textContent = `Spice Rating: ${book.spiceRating}`;
+
+    const excitementRatingElement = document.createElement('p');
+    excitementRatingElement.textContent = `Excitement Rating: ${book.excitementRating}`;
+
+    const overallRatingElement = document.createElement('p');
+    overallRatingElement.textContent = `Overall Rating: ${book.overallRating}`;
 
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('delete-btn');
@@ -72,7 +93,7 @@ function displayBooks() {
     editBtn.addEventListener('click', () => {
       titleInput.value = book.title;
       authorInput.value = book.author;
-      descInput.value = book.descripion;
+      descInput.value = book.description;
       statusInput.value = book.status;
 
       // Remove the current book from the library
@@ -81,10 +102,11 @@ function displayBooks() {
       displayBooks();
     });
 
-    bookDiv.append(title, author, description, status, editBtn, deleteBtn);
+    bookDiv.append(title, author, description, status, tearRatingElement, spiceRatingElement, excitementRatingElement, overallRatingElement, editBtn, deleteBtn);
     bookList.appendChild(bookDiv);
   });
 }
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
